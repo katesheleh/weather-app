cf031ed5c8e49dfff863c6354478a1a29b5f0d08
katesheleh <kate.sheleh@gmail.com>
2020-10-26 19:54:11 +0100

    Updates

---
+++ precache-manifest.8b7cc6e4ee3305a6a641fd2b2c23bf9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56717cd571ef4471782e1d5743b527fc",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "7320aaf01e4b2f6e87c9",
+    "url": "/weather-app/static/css/main.f1a12aae.chunk.css"
+  },
+  {
+    "revision": "0809e70b2914a1b8833f",
+    "url": "/weather-app/static/js/2.42bf394c.chunk.js"
+  },
+  {
+    "revision": "4df4c58fd3db8af31000da1d75d059ea",
+    "url": "/weather-app/static/js/2.42bf394c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7320aaf01e4b2f6e87c9",
+    "url": "/weather-app/static/js/main.742ad2b0.chunk.js"
+  },
+  {
+    "revision": "3565ad6a1d257d180595",
+    "url": "/weather-app/static/js/runtime-main.b0a5adf8.js"
+  },
+  {
+    "revision": "aa8b6d134ec17489e23af525c1f191a6",
+    "url": "/weather-app/static/media/loader.aa8b6d13.svg"
+  }
+]);
